fd1e95fa9eb1e8fa6f09c89b291f7096d32f0b28
lembn <lembn@outlook.com>
2022-02-11 12:25:18 +0000

    better error handling and global installation

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # freez
 
-Create single-file executables from python scripts ony any python-running system. `freez` takes the entry point script for a program and freezes it into a single-file executable. The entry point script of the program being built should be in a different directory to `freez`'s `main.py`. `freez` has no external dependencies, so can be used on any system with a python interpreter and can build any python program, regardless of the dependencies within it, wihtout requiring the dependencies being installed on the machine. `freez` requries an internet connection to build programs so that dependencies can be fetched from external locations.
+Create single-file executables from python programs ony any python-running system. `freez` takes the entry point script for a program and freezes the entire program it into a single-file executable using [PyInstaller](https://github.com/pyinstaller/pyinstaller). `freez` has no external dependencies, so can be used on any system with a python interpreter and can build any python program, regardless of the dependencies within it. `freez` requries an internet connection to build programs so that dependencies can be fetched from external locations. `freez` uses to freeze programs, providing the main functionality of the program.
 
-`freez` uses [PyInstaller](https://github.com/pyinstaller/pyinstaller) to freeze programs, providing the main functionality of the program.
+> The entry point script of the program being built should be away from the `freez` directory.
+
+## Why not use normal PyInstaller?
+
+PyInstaller will fail if you try to build a program that uses external dependencies which you do not have installed on your system. All `freez` does is collect these dependencies and install them into a virtual environment, allowing PyInstaller to build in that environment and also allowing the installed dependencies to be easily cleaned off the system as if they were never there. This way any python program can be built into an executable from it's source code without the build machine having any of the program's dependencies pre-installed.
+
+## Requirements
+
+- Python >= 3.6
+- Pip must be in the system `PATH`
 
 ## Usage
 
 ```
-Usage: python freez [OPTIONS] ENTRY
+Usage: [python] freez [OPTIONS] ENTRY
+
+  Create single-file executables from python scripts.
 
   ENTRY: The entry point script of the program being built.
 
@@ -16,13 +27,23 @@ Options:
                           ./]
   -n, --name TEXT         Name of the built executable (defaults to name of
                           entry point script).
+  -g, --global            Install the executable into the python script path
+                          to make it globally accessible across the system
+                          (this will ignore the path set by --output).
   --help                  Show this message and exit.
 ```
 
-## Roadmap
+> Global installations require 'sudo' on Unix or 'Run as administrator' on Windows
 
-- [ ] Make the program globally callable on the system (without needing to specify `python`)
-  - Add the package to sys.path
-  - Make package executable my adding a `__main__` file
-  - Add option to make packages created with this program globally callable
-- [ ] Handle fails more gracefully (perform cleanup on opened/created files and virtual environments)
+## Installation
+
+Since `freez` is a python program, it can actually be used to install itself globally across the system:
+
+```
+> cd freez
+> python main.py -g -n freez main.py
+> freez --version
+freez, version 1.2.0
+```
+
+This builds `freez` as an executable, meaning it can be run without invoking `python` anywhere on the system.
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import subprocess
 import os
 import shutil
 import platform
+import sys
 from typing import Callable
 from datetime import datetime
 import vendor.click as click
@@ -18,7 +19,13 @@ def log(message: str, type: str = "INFO", colour: str = "white") -> None:
         )
     )
 
+def delete(path: str, _dir: bool = True):
+    remove = shutil.rmtree if _dir else os.remove
+    if (os.path.exists(path)):
+        remove(path)
+
 @click.command()
+@click.version_option("1.2.0")
 @click.argument("entry", type=click.Path(exists=True, dir_okay=False))
 @click.option(
     "-o",
@@ -34,7 +41,14 @@ def log(message: str, type: str = "INFO", colour: str = "white") -> None:
     type=click.STRING,
     help="Name of the built executable (defaults to name of entry point script).",
 )
-def cli(entry: str, output: str, name: str):
+@click.option(
+    "-g",
+    "--global",
+    "_global",
+    is_flag = True,
+    help="Install the executable into the python script path to make it globally accessible across the system (this will ignore the path set by --output).",
+)
+def cli(entry: str, output: str, name: str, _global: bool) -> None:
     """
     Create single-file executables from python scripts.
 
@@ -42,8 +56,9 @@ def cli(entry: str, output: str, name: str):
     ENTRY: The entry point script of the program being built.
     """
 
-    remove = False
+    remove_pipenv = False
 
+    print()
     log("Checking required global dependency tools...")
     try:
         subprocess.run(["pipenv", "--version"])
@@ -56,54 +71,67 @@ def cli(entry: str, output: str, name: str):
             log("Found pip.")
             log(f"Installing pipenv (it will be removed later)...")
             subprocess.run(["pip3", "install", "pipenv"])
-            remove = True
+            remove_pipenv = True
         except FileNotFoundError:
             log("Failed to find pip.", type="ERROR", colour="RED")
             log("Unable to install requried dependencies\nAborting...")
             return
 
+    print()
     log("Installing freez dependencies...")
-    requirements = join(os.path.dirname(os.path.abspath(__file__)), "requirements.txt")
-    subprocess.run(["pipenv", "install", "-r", requirements, "--skip-lock"])
+    subprocess.run(["pipenv", "install", "--skip-lock", "pipreqs", "pyinstaller"])
 
-    shutil.copy("./Pipfile", "./.Pipfile")
-    args = ["pipenv", "run", "pipreqs", "--force"]
-    entry_parent = os.path.dirname(os.path.abspath(entry))
-    args.append(entry_parent)
-    log(f"Collecting {entry} dependencies...")
-    subprocess.run(args)
-    log(f"Installing {entry} dependencies...")
-    requirements = join(entry_parent, "requirements.txt")
-    subprocess.run(["pipenv", "install", "-r", requirements, "--skip-lock"])
-    log("Dependencies installed")
-    os.remove(f"./Pipfile")
-    os.remove(requirements)
-    os.replace("./.Pipfile", "./Pipfile")
+    try:
+        print()
+        log(f"Collecting {entry} dependencies...")
+        args = ["pipenv", "run", "pipreqs", "--force"]
+        entry_parent = os.path.dirname(os.path.abspath(entry))
+        args.append(entry_parent)
+        subprocess.run(args)
 
-    if not name:
-        name = re.sub(r"([\.\w]+[\\/])+", "", entry)
-        name = re.sub("(\.py)", "", name)
-    if platform.system().upper() == "WINDOWS":
-        name += ".exe"
+        print()
+        log(f"Installing {entry} dependencies...")
+        requirements = join(entry_parent, "requirements.txt")
+        subprocess.run(["pipenv", "install", "-r", requirements, "--skip-lock"])
 
-    subprocess.run(["pipenv", "run", "pyinstaller", entry, "--onefile", "--name", name])
-    os.remove(f"./{name}.spec")
-    shutil.rmtree("./build")
-    if not os.path.exists(output):
-        os.makedirs(output)
-    os.replace(f"./dist/{name}", join(output, name))
-    shutil.rmtree("./dist")
-    pycache = "__pycache__"
-    if os.path.exists(pycache):
-        shutil.rmtree(pycache)
-    entry_pycache = join(entry_parent, pycache)
-    if os.path.exists(entry_pycache):
-        shutil.rmtree(entry_pycache)
+        print()
+        log("Dependencies installed.")
+        delete(f"./Pipfile", False)
+        os.remove(requirements)
 
-    subprocess.run(["pipenv", "--rm"])
-
-    if remove:
-        subprocess.run(["pip3", "uninstall", "-y", "pipenv"])
+        if _global:
+            output = join(os.path.dirname(sys.executable), "Scripts")
+        elif not os.path.exists(output):
+            os.makedirs(output)
+        log(f"Building executable to {output}")
+        if not name:
+            name = re.sub(r"([\.\w]+[\\/])+", "", entry)
+            name = re.sub("(\.py)", "", name)
+        if platform.system().upper() == "WINDOWS":
+            name += ".exe"
+        subprocess.run(["pipenv", "run", "pyinstaller", entry, "--onefile", "--name", name])
+        shutil.move(f"./dist/{name}", join(output, name))
+    except PermissionError as e:
+        print()
+        log(e, type="ERROR", colour="red")
+        log("Global installations require 'sudo' on Unix or 'Run as administrator' on Windows")
+    except KeyboardInterrupt:
+        print()
+        log("Aborted!")
+    finally:
+        print()
+        log("Cleaning up...")
+        delete(f"./{name}.spec", False)
+        delete("./build")
+        delete("./dist")
+        pycache = "__pycache__"
+        delete(pycache)
+        entry_pycache = join(entry_parent, pycache)
+        delete(entry_pycache)
+        log("Artefacts removed.")
+        subprocess.run(["pipenv", "--rm"])
+        if remove_pipenv:
+            subprocess.run(["pip3", "uninstall", "-y", "pipenv"])
 
 
 if __name__ == "__main__":
