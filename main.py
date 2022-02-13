c9b111930b2acd0e7f4cabf01a968f526c7736a7
lembn <lembn@outlook.com>
2022-02-13 00:29:43 +0000

    Added better exit logs

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def delete(path: str, _dir: bool = True):
         remove(path)
 
 @click.command()
-@click.version_option("1.3.1")
+@click.version_option("1.3.2")
 @click.argument("entry", type=click.Path(exists=True, dir_okay=False))
 @click.option(
     "-o",
@@ -135,13 +135,16 @@ def cli(entry: str, output: str, name: str, _global: bool) -> None:
         entry_pycache = join(entry_parent, pycache)
         delete(entry_pycache)
         if replace_pipfile:
-            os.rename("./Pipfile.STORE", "./Pipfile")
             subprocess.run(["pipenv", "--rm"])
+            log("Reconstructing original virutal environment...")
             subprocess.run(["pipenv", "install"])
+            os.rename("./Pipfile.STORE", "./Pipfile")
+            log("Environment restored.")
         log("Artefacts removed.")
         if remove_pipenv:
+            log("Removing pipenv")
             subprocess.run(["pip3", "uninstall", "-y", "pipenv"])
-
+        log("Build successful.")
 
 if __name__ == "__main__":
     cli(prog_name="freez")
